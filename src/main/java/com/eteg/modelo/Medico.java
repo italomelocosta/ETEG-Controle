ef061a0a9875f8e0f898ec98d36e31733b599aa7
italomelocosta <italomelocosta@gmail.com>
2017-12-15 09:15:00 -0200

    Commit

--- src/main/java/com/eteg/modelo/Medico.java
+++ src/main/java/com/eteg/modelo/Medico.java
@@ -8,17 +8,7 @@ package com.eteg.modelo;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
